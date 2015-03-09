3048f99f8e1336c95f5b67366db60dc70942b5c4
dsnam <ska465@naver.com>
2015-03-09 12:32:30 +0900

    MyListener add

--- src/test/java/webapp/service/DeptInfoServiceTest.java
+++ src/test/java/webapp/service/DeptInfoServiceTest.java
@@ -9,8 +9,10 @@ import org.apache.log4j.Logger;
 import org.hamcrest.core.Is;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -26,83 +28,71 @@ import webapp.util.EmployeeDataSource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/webapp/spring/beans.xml")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DeptInfoServiceTest {
 
 	static Logger log = Logger.getLogger(DeptInfoServiceTest.class);
-	
+
 	@Autowired
-	ApplicationContext factory;	
-	
+	ApplicationContext factory;
+
 	@Test
-	public void testGetDeptInfo() {
+	public void test1_GetDeptInfo() {
 		log.info("###@@@ testGetDeptInfo()");
-		
+
 		DeptInfoService service = factory.getBean(DeptInfoService.class);
-		
+
 		Dept dept = service.getDeptInfo(10);
 		assertNotNull(dept);
 		log.info("deptno=" + dept.getDeptno());
 		log.info("dname=" + dept.getDname());
 		log.info("loc=" + dept.getLoc());
-		
+
 	}
-	
+
 	@Test
-	public void testGetDeptInfoWithEmps() {
+	public void test2_GetDeptInfoWithEmps() {
 		log.info("###@@@ testGetDeptInfoWithEmps()");
-		
+
 		DeptInfoService service = factory.getBean(DeptInfoService.class);
-		
+
 		Dept dept = service.getDeptInfoWithEmps(10);
 		assertNotNull(dept);
-		
+
 		log.info(dept.getDeptno() + " " + dept.getDname());
-		
-		List<Emp> emps =  dept.getEmps();
+
+		List<Emp> emps = dept.getEmps();
 		if (emps != null) {
 			for (Emp e : emps) {
 				log.info(e.getEmpno() + " " + e.getEname());
 			}
 		}
 	}
-	
+
 	@Test
-	public void testGetDeptInfoAll() {
+	public void test3_GetDeptInfoAll() {
 		DeptInfoService service = factory.getBean(DeptInfoService.class);
 		List<Dept> list = service.getDeptInfoAll();
 		assertNotNull(list);
-		
+
 		for (Dept d : list) {
 			log.info(d.getDeptno() + " " + d.getDname() + " " + d.getLoc());
 		}
 	}
-	
-	
+
+	@Test
+	public void test4_GetDeptInfoAllWithEmps() {
+
+		DeptInfoService service = factory.getBean(DeptInfoService.class);
+		List<Dept> list = service.getDeptInfoAllWithEmps();
+		assertNotNull(list);
+
+		for (Dept d : list) {
+			log.info(d.getDeptno() + " " + d.getDname() + " " + d.getLoc());
+			for (Emp e : d.getEmps())
+				log.info(e.getEmpno() + " " + e.getEname() + " " + e.getJob() + " " + e.getMgr() + " " + e.getHiredate() + " " + e.getSal() + " " + e.getComm());
+			
+		}
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
